bc77fd9e2b3fb31395dfd49079b04896562a2948
KelvinP25 <kelvincastillo9911@gmail.com>
2021-10-05 23:19:46 -0300

    Creando el disenio de Consulta

---
+++ UI/Consulta/cConsulta.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_AP1_Kelvin_20180193.UI.Consulta
+{
+    /// <summary>
+    /// Interaction logic for cConsulta.xaml
+    /// </summary>
+    public partial class cConsulta : Window
+    {
+        public cConsulta()
+        {
+            InitializeComponent();
+        }
+    }
+}
