bb275885d44595365a6d8ff02d6f470929166502
KelvinP25 <kelvincastillo9911@gmail.com>
2021-10-05 22:33:07 -0300

    Creando la entidad Aportes

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P1_AP1_Kelvin_20180193.Entidades
+{
+    public class Aportes
+    {
+        [Key]
+        public int AporteId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Persona { get; set; }
+        public string Concepto { get; set; }
+        public float Monto { get; set; }
+    }
+}
