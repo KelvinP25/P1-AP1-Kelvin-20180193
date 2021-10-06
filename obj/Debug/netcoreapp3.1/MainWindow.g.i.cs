be6f1cafa7bb7d89d871fb33ed489cb7936c2ec1
KelvinP25 <kelvincastillo9911@gmail.com>
2021-10-05 22:38:10 -0300

    Creando la Migracion

--- obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace P1_AP1_Kelvin_20180193 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/P1-AP1-Kelvin-20180193;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/P1-AP1-Kelvin-20180193;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
