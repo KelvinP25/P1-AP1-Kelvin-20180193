4627917f07e7f5f1133328d318bb0f193373dfdf
KelvinP25 <kelvincastillo9911@gmail.com>
2021-10-05 22:42:42 -0300

    Creando UtilidadesBLL en la BLL

---
+++ BLL/UtilidadesBLL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P1_AP1_Kelvin_20180193.BLL
+{
+    public class UtilidadesBLL
+    {
+        public static int ToInt(string valor)
+        {
+            int retorno = 0;
+
+            int.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+    }
+}
