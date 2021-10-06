98077b96591cce788720a83acd6225e01ec9cab5
KelvinP25 <kelvincastillo9911@gmail.com>
2021-10-05 22:35:45 -0300

    Creando contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_Kelvin_20180193.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P1_AP1_Kelvin_20180193.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> aportes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\Aportes.db");
+        }
+    }
+}
