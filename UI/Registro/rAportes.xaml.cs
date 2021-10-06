450a010559e5a1251d79ae32339519c475411c10
KelvinP25 <kelvincastillo9911@gmail.com>
2021-10-05 22:57:51 -0300

    Creando el disenio del Registro

---
+++ UI/Registro/rAportes.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_AP1_Kelvin_20180193.UI.Registro
+{
+    /// <summary>
+    /// Interaction logic for rAportes.xaml
+    /// </summary>
+    public partial class rAportes : Window
+    {
+        public rAportes()
+        {
+            InitializeComponent();
+        }
+    }
+}
